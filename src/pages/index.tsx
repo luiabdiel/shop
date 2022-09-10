0cda0791c728597c94dd8e2da9a827d350185e47
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-09-09 23:17:24 -0300

    feat: carousel with lib keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import { HomeContainer, Product } from "../styles/pages/home";
 
+import { useKeenSlider } from "keen-slider/react"
+
 import shirtOne from "../assets/shirt/01.png"
 import shirtTwo from "../assets/shirt/02.png"
 import shirtThree from "../assets/shirt/03.png"
 
+import "keen-slider/keen-slider.min.css"
+
 export default function Home() {
+  const [ sliderRef ] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image 
           src={shirtOne} 
           width={520} 
@@ -21,7 +32,7 @@ export default function Home() {
           </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image 
           src={shirtTwo} 
           width={520} 
@@ -34,7 +45,20 @@ export default function Home() {
           </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image 
+          src={shirtThree} 
+          width={520} 
+          height={480} 
+          alt="" />
+
+          <footer>
+            <strong>Camisa X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
         <Image 
           src={shirtThree} 
           width={520} 
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,17 +3,17 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
     display: "flex",
-    gap: "3rem",
 
     width: "100%",
     maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
     marginLeft: "auto",
+    
+    minHeight: 554,
 })
 
 export const Product = styled("a", {
     background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
     borderRadius: 8,
-    padding: ".25rem",
 
     cursor: "pointer",
 
@@ -24,7 +24,7 @@ export const Product = styled("a", {
     justifyContent: "center",
 
     overflow: "hidden",
-
+    
     img: {
 
         objectFit: "cover"
