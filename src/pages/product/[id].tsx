a417fb02ba927398f1e334d8c26a9e83db7a129a
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-09-12 12:58:51 -0300

    feat: product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,28 @@
+import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+
 export default function Product() {
+    const { query } = useRouter()
+
     return (
-        <h1>Product</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camisa x</h1>
+                <span>R$ 79,90</span>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Adipisci, pariatur quia. Excepturi, consequatur nemo. 
+                    Praesentium, sequi eveniet! Architecto blanditiis quam, 
+                    consequuntur dicta quibusdam culpa molestias nostrum id 
+                    numquam quisquam neque.
+                </p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
