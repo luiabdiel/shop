923b80c7a15ecd66b6c7a870429d5267eb7b2d1d
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-09-14 14:44:35 -0300

    fix: purchase data on success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -31,23 +31,23 @@ export default function Success({ costumerName, product }: SuccessProps) {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ query, params }) => {
-    const sessionId = String(query.session_id)
-
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const sessionId = String(query.session_id);
+  
     const session = await stripe.checkout.sessions.retrieve(sessionId, {
-        expand: ["line_items", "line_items.data.price.product"]
-    })
-
-    const customerName = session.customer_details.name
-    const product = session.line_items.data[0].price.product as Stripe.Product
-
+      expand: ['line_items', 'line_items.data.price.product']
+    });
+  
+    const costumerName = session.customer_details.name;
+    const product = session.line_items.data[0].price.product as Stripe.Product;
+  
     return {
-        props: {
-            customerName,
-            product: {
-                name: product.name,
-                ImageUrl: product.images[0],
-            }
+      props: {
+        costumerName,
+        product: {
+          name: product.name,
+          imageUrl: product.images[0]
         }
+      }
     }
-}
+  }
